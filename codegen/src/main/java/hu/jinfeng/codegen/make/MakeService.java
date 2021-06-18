99d6edb426ba9eb625a1a862d90aa5257f9bc69f
hujinfeng <hujinfeng@yscredit.com>
2021-06-18 21:57:05 +0800

    insert

--- codegen/src/main/java/hu/jinfeng/codegen/make/MakeService.java
+++ codegen/src/main/java/hu/jinfeng/codegen/make/MakeService.java
@@ -33,27 +33,33 @@ public class MakeService {
         vmContext.put("entityClassName", entityClassName);
         //1，生产entity对象
         if (null != makeContext.getEntityPackage()) {
-            String vm = FileUtils.readLocalFile(VelocityEngineUtils.LOCAL_RESOURCE_PATH + "template/java/entity.vm");
-            String content = VelocityEngineUtils.parseTemplate(vm, vmContext);
-            String path = makeCodeConfiguration.getCodeOutputPath() +
-                    FileUtils.package2Path(makeContext.getEntityPackage()) + "/" + entityClassName + ".java";
-            FileUtils.writeFile(path, content);
+            String vm = FileUtils.readLocalFile(VelocityEngineUtils.LOCAL_RESOURCE_PATH + "original/java/entity.vm");
+            if(null != vm) {
+                String content = VelocityEngineUtils.parseTemplate(vm, vmContext);
+                String path = makeCodeConfiguration.getCodeOutputPath() +
+                        FileUtils.package2Path(makeContext.getEntityPackage()) + "/" + entityClassName + ".java";
+                FileUtils.writeFile(path, content);
+            }
         }
         //2, 生成mapper对象
         if (null != makeContext.getMapperPackage()) {
-            String vm = FileUtils.readLocalFile(VelocityEngineUtils.LOCAL_RESOURCE_PATH + "template/java/mapper.vm");
-            String content = VelocityEngineUtils.parseTemplate(vm, vmContext);
-            String path = makeCodeConfiguration.getCodeOutputPath() +
-                    FileUtils.package2Path(makeContext.getMapperPackage()) + "/" + entityClassName + "Mapper.java";
-            FileUtils.writeFile(path, content);
+            String vm = FileUtils.readLocalFile(VelocityEngineUtils.LOCAL_RESOURCE_PATH + "original/java/mapper.vm");
+            if(null != vm) {
+                String content = VelocityEngineUtils.parseTemplate(vm, vmContext);
+                String path = makeCodeConfiguration.getCodeOutputPath() +
+                        FileUtils.package2Path(makeContext.getMapperPackage()) + "/" + entityClassName + "Mapper.java";
+                FileUtils.writeFile(path, content);
+            }
         }
         //3, 生成service
         if (null != makeContext.getServicePackage()) {
-            String vm = FileUtils.readLocalFile(VelocityEngineUtils.LOCAL_RESOURCE_PATH + "template/java/service.vm");
-            String content = VelocityEngineUtils.parseTemplate(vm, vmContext);
-            String path = makeCodeConfiguration.getCodeOutputPath() +
-                    FileUtils.package2Path(makeContext.getServicePackage()) + "/" + entityClassName + "Service.java";
-            FileUtils.writeFile(path, content);
+            String vm = FileUtils.readLocalFile(VelocityEngineUtils.LOCAL_RESOURCE_PATH + "original/java/service.vm");
+            if(null != vm) {
+                String content = VelocityEngineUtils.parseTemplate(vm, vmContext);
+                String path = makeCodeConfiguration.getCodeOutputPath() +
+                        FileUtils.package2Path(makeContext.getServicePackage()) + "/" + entityClassName + "Service.java";
+                FileUtils.writeFile(path, content);
+            }
         }
 
 
