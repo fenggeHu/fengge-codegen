6c78bfb49cd150fc0904e2e7f68f1e4c978a0724
hujinfeng <pbfoxhu@gmail.com>
2021-12-15 14:50:09 +0800

    修改pager注释

--- codegen/src/main/java/hu/jinfeng/plugin/Pager.java
+++ codegen/src/main/java/hu/jinfeng/plugin/Pager.java
@@ -1,16 +1,24 @@
 package hu.jinfeng.plugin;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import java.io.Serializable;
 
 /**
  * @Description: 简单的计算分页
  * @Author Jinfeng.hu  @Date 2021-11-01
  **/
+@ApiModel("分页")
 public class Pager implements Serializable {
     private static final long serialVersionUID = 1L;
+    @ApiModelProperty("页码")
     private int pageNum;
+    @ApiModelProperty("pageSize")
     private int pageSize;
+    @ApiModelProperty(value = "起始行", hidden = true)
     private int startRow;
+    @ApiModelProperty(value = "结束行", hidden = true)
     private int endRow;
 
     public Pager(int pageNum, int pageSize) {
@@ -19,9 +27,13 @@ public class Pager implements Serializable {
         this.calculateStartAndEndRow();
     }
 
-    private void calculateStartAndEndRow() {
+    public void calculateStartAndEndRow() {
         this.startRow = this.pageNum > 0 ? (this.pageNum - 1) * this.pageSize : 0;
         this.endRow = this.startRow + this.pageSize * (this.pageNum > 0 ? 1 : 0);
+    }
+
+    public static Pager defaultPager() {
+        return new Pager(1, 20);
     }
 
     public static Pager getPager(int pageNum, int pageSize) {
