b83caab2a97884ca3c80b2aa7a6afadd9f18bc25
hujinfeng <pbfoxhu@gmail.com>
2022-04-13 14:33:38 +0800

    预留的

---
+++ codegen/src/test/java/fengge/FormatSourceTest.java
@@ -0,0 +1,18 @@
+package fengge;
+
+import com.google.googlejavaformat.java.Formatter;
+import hu.jinfeng.commons.utils.FileUtils;
+import lombok.SneakyThrows;
+
+/**
+ * @Description:
+ * @Author Jinfeng.hu  @Date 2022/3/17
+ **/
+public class FormatSourceTest {
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        String content = FileUtils.readLocalFile("/Users/max/opensource/jinfeng-stock/jinfeng-stock-common/src/main/java/hu/jinfeng/stock/common/talib/Core.java");
+        FileUtils.writeFile("/Users/max/core_java.txt", new Formatter().formatSource(content));
+    }
+}
