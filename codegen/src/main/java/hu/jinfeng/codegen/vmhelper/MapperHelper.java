824896f9a009bc56ae2c7f253ba016cf8c97d23f
hujinfeng <pbfoxhu@gmail.com>
2022-08-12 11:17:04 +0800

    更新mapper查询模板

--- codegen/src/main/java/hu/jinfeng/codegen/vmhelper/MapperHelper.java
+++ codegen/src/main/java/hu/jinfeng/codegen/vmhelper/MapperHelper.java
@@ -4,17 +4,31 @@ import hu.jinfeng.codegen.model.ColumnInfo;
 import hu.jinfeng.codegen.model.TableInfo;
 import hu.jinfeng.commons.utils.NameStringUtils;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @Description: 构建mybatis mapper的元素
  * @Author Jinfeng.hu  @Date 2022/8/9
  **/
 public class MapperHelper {
+    /**
+     * 字符太长了加入换行
+     */
+    private int newLine(final StringBuilder sb, int start) {
+        if (sb.length() - start > 100) {
+            sb.append("\"+\n\"");
+            start = sb.length();
+        }
+        return start;
+    }
 
     public String insertColumns(final TableInfo tableInfo) {
         StringBuilder sb = new StringBuilder();
-
+        int start = 0;
         for (ColumnInfo col : tableInfo.getInsertColumns()) {
             if (col.isAutoIncrement()) continue;
+            start = newLine(sb, start); // 太长了换行
             if (sb.length() > 0) sb.append(", ");
             sb.append("`").append(col.getName()).append("`");
         }
@@ -24,9 +38,10 @@ public class MapperHelper {
 
     public String insertValues(final TableInfo tableInfo) {
         StringBuilder sb = new StringBuilder();
-
+        int start = 0;
         for (ColumnInfo col : tableInfo.getInsertColumns()) {
             if (col.isAutoIncrement()) continue;
+            start = newLine(sb, start);
             if (sb.length() > 0) sb.append(", ");
             sb.append("#{").append(NameStringUtils.toPropertyName(col.getName())).append("}");
         }
@@ -36,9 +51,10 @@ public class MapperHelper {
 
     public String batchInsertValues(final TableInfo tableInfo) {
         StringBuilder sb = new StringBuilder();
-
+        int start = 0;
         for (ColumnInfo col : tableInfo.getInsertColumns()) {
             if (col.isAutoIncrement()) continue;
+            start = newLine(sb, start); // 太长了换行
             if (sb.length() > 0) sb.append(", ");
             sb.append("#{it.").append(NameStringUtils.toPropertyName(col.getName())).append("}");
         }
@@ -48,15 +64,28 @@ public class MapperHelper {
 
     public String onDuplicateKeyUpdate(final TableInfo tableInfo) {
         StringBuilder sb = new StringBuilder();
-
+        int start = 0;
         for (ColumnInfo col : tableInfo.getInsertColumns()) {
             // update不包含自增和unique key、分库分表字段
             if (col.isAutoIncrement() || tableInfo.getUkNames().contains(col.getName())
                     || tableInfo.getShardingNames().contains(col.getName())) continue;
+            start = newLine(sb, start); // 太长了换行
             if (sb.length() > 0) sb.append(", ");
             sb.append("`").append(col.getName()).append("`=VALUES(").append("`").append(col.getName()).append("`)");
         }
 
+        return sb.toString();
+    }
+
+    public String updateSet(final TableInfo table) {
+        StringBuilder sb = new StringBuilder();
+        int start = 0;
+        List<String> names = table.getUpdateColumns().stream().map(e -> e.getName()).collect(Collectors.toList());
+        for (String col : names) {
+            start = newLine(sb, start); // 太长了换行
+            if (sb.length() > 0) sb.append(", ");
+            sb.append("`").append(col).append("` = #{").append(NameStringUtils.toPropertyName(col)).append("} ");
+        }
         return sb.toString();
     }
 
